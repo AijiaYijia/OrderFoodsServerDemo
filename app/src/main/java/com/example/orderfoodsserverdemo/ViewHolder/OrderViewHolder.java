50d14bc87a1b46fa7d60f73464de7361cc87533f
Dong ZHAO <east_hair@hotmail.com>
2019-06-24 12:31:48 +1200

    notification

---
+++ app/src/main/java/com/example/orderfoodsserverdemo/ViewHolder/OrderViewHolder.java
@@ -0,0 +1,48 @@
+package com.example.orderfoodsserverdemo.ViewHolder;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ContextMenu;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.orderfoodsserverdemo.Interface.ItemClickListener;
+import com.example.orderfoodsserverdemo.R;
+
+public class OrderViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnCreateContextMenuListener {
+
+    public TextView txtOrderId, txtOrderStatus, txtOrderPhone, txtOrderAddress;
+    private ItemClickListener itemClickListener;
+
+    public OrderViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        txtOrderId = itemView.findViewById(R.id.order_id);
+        txtOrderStatus = itemView.findViewById(R.id.order_status);
+        txtOrderPhone = itemView.findViewById(R.id.order_phone);
+        txtOrderAddress = itemView.findViewById(R.id.order_address);
+
+        itemView.setOnClickListener(this);
+        itemView.setOnCreateContextMenuListener(this);
+    }
+
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+
+        this.itemClickListener = itemClickListener;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        itemClickListener.onClick(v, getAdapterPosition(), false);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+
+        menu.setHeaderTitle("Select the Action");
+
+        menu.add(0, 0, getAdapterPosition(), "Update");
+        menu.add(0, 1, getAdapterPosition(), "Delete");
+    }
+}
